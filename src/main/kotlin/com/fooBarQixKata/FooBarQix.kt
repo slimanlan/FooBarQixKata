b24cf9aecb294fe0a06bdee2a9a73860bbe6dd79
LANSARI Slimane <slimane.lansari@talan.com>
2020-09-18 16:55:34 +0200

    refactoring

--- src/main/kotlin/com/fooBarQixKata/FooBarQix.kt
+++ src/main/kotlin/com/fooBarQixKata/FooBarQix.kt
@@ -1,44 +1,19 @@
 package com.fooBarQixKata
 
 class FooBarQix {
-	
-	val fooBarQixhashMap:HashMap<Int,String> = HashMap<Int,String>() 
 
-	init {
-		fooBarQixhashMap.put(3,"Foo")  
-		fooBarQixhashMap.put(5,"Bar")  
-		fooBarQixhashMap.put(7,"Qix")
-	}
-	
-	fun generateFooBarQix(numberToFoobar:Int):String {
-		
-		  var result = ""
-		  if (numberToFoobar % 3 == 0)
-		  {
-		    result += fooBarQixhashMap.get(3)
-		  }
-		  if (numberToFoobar % 5 == 0)
-		  {
-		    result += fooBarQixhashMap.get(5)
-		  }
-		  if (numberToFoobar % 7 == 0)
-		  {
-		    result += fooBarQixhashMap.get(7)
-		  }
-		
-		  val toFooBar = (numberToFoobar).toString()
-		
-		  for (i in 0 until toFooBar.length)
-		  {
-		    val digit = toFooBar.get(i)
-		    if (digit == '3')
-		    result += fooBarQixhashMap.get(3)
-		    if (digit == '5')
-		    result += fooBarQixhashMap.get(5)
-		    if (digit == '7')
-		    result += fooBarQixhashMap.get(7)
-		  }
-		
-		  return if (result.isEmpty()) toFooBar else result
-	}
+    private val words = mapOf(3 to "Foo", 5 to "Bar", 7 to "Qix")
+
+    fun generateFooBarQix(number: Int): String {
+
+       var result = words
+               .map { if (number % it.key == 0) it.value else "" }
+               .joinToString("")
+
+        number.toString().forEach {
+           result += words.getOrDefault( Character.getNumericValue(it), "")
+        }
+
+        return if(result.isEmpty()) number.toString() else result
+    }
 }
